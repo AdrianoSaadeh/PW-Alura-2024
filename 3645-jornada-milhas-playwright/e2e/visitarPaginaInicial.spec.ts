bcb790066ff14ea3017da0eb4867a6aafe9e76a9
Adriano Saadeh <adrianosaadeh@gmail.com>
2024-12-10 08:03:07 -0300

    primeiro teste

---
+++ 3645-jornada-milhas-playwright/e2e/visitarPaginaInicial.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test.describe("Página Inicial", () => {
+    test("Deve visitar a página inicial", async ({ page }) => {
+        await page.goto("/");
+        await expect(page).toHaveTitle('Jornada Milhas');
+
+        //const tituloPassagens = page.getByRole('heading', { name: 'Passagens' });
+        const tituloPassagens = page.getByTestId('titulo-passagens')
+        await expect(tituloPassagens).toBeVisible();
+
+        const tituloPromocoes = page.getByTestId('titulo-promocoes')
+        await expect(tituloPromocoes).toBeVisible();
+
+        const tituloDepoimentos = page.getByTestId('titulo-depoimentos')
+        await expect(tituloDepoimentos).toBeVisible();
+    });
+});
